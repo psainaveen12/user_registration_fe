313c97859d0fb88c613db99e8b0479591ef52c56
psainaveen12 <puttisainaveen@gmail.com>
2023-09-05 16:12:25 -0400

    users grid

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import AddUser from './users/AddUser';
 import EditUser from './users/EditUser';
 import ViewUser from './users/ViewUser';
+import HomeUserProfiles from './pages/HomeUserProfiles';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Navbar/>
         <Routes>
           <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/HomeUserProfiles" element={<HomeUserProfiles/>}/>
           <Route exact path="/adduser" element={<AddUser/>}/>
           <Route exact path="/edituser/:id" element={<EditUser/>}/>
           <Route exact path="/viewuser/:id" element={<ViewUser/>}/>
