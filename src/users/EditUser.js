3fb2d821fb7c85f6b9d834abd759c687819c8f6f
psainaveen12 <puttisainaveen@gmail.com>
2023-10-02 10:44:11 -0400

    updated proxy

--- src/users/EditUser.js
+++ src/users/EditUser.js
@@ -1,113 +1,118 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark, faArrowRightToBracket} from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faXmark,
+  faArrowRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function EditUser() {
+  let navigate = useNavigate();
 
-    const ROOT_URL = "http://localhost:8081/";
-    let navigate = useNavigate();
+  const { id } = useParams();
 
-    const {id} = useParams()
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+    phonenumber: null,
+    date: null,
+    time: null,
+  });
 
-    const [user, setUser]=useState({
-        name:"",
-        username:"",
-        email:"",
-        phonenumber:null,
-        date:null,
-        time:null
-    });
+  const { name, username, email, phonenumber, date, time } = user;
 
-    const{name,username,email,phonenumber,date,time}=user
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
-    const onInputChange=(e)=>{
-        setUser({...user, [e.target.name]:e.target.value})
-    }
+  useEffect(() => {
+    loadUser();
+  }, []);
 
-    useEffect(()=>{
-        loadUser()
-    },[])
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.put(`/updateuser/${id}`, user);
+    navigate("/");
+  };
 
-    const onSubmit=async (e)=>{
-        e.preventDefault();
-        await axios.put(ROOT_URL+`updateuser/${id}`,user);
-        navigate("/");
-    };
-
-    const loadUser = async () => {
-        const result = await axios.get(ROOT_URL+`user/${id}`)
-        setUser(result.data)
-    }
+  const loadUser = async () => {
+    const result = await axios.get(`/user/${id}`);
+    setUser(result.data);
+  };
 
   return (
-    <div className='conntainer'>
-        <div className='row'>
-            <div className="col-md-6 offset-3 border rounded p-4 mt-2 shadow custom_table_style">
-                <h2 className='text-center m-4'>Edit User</h2>
-                <form onSubmit={(e)=>onSubmit(e)}>
-                    <div className='mb-3'>
-                        <label htmlFor='Name' className='form-lable'>
-                        Name
-                        </label>
-                        <input
-                        type={'text'}
-                        className='form-control'
-                        placeholder='Enter Your Name'
-                        name = "name"
-                        value={name}
-                        onChange={(e)=>onInputChange(e)}
-                        required
-                        />
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor='Username' className='form-lable'>
-                        UserName
-                        </label>
-                        <input
-                        type={'text'}
-                        className='form-control'
-                        placeholder='Enter Your UserName'
-                        name = "username"
-                        value={username}
-                        onChange={(e)=>onInputChange(e)}
-                        required
-                        />
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor='Email' className='form-lable'>
-                        Email
-                        </label>
-                        <input
-                        type={'text'}
-                        className='form-control'
-                        placeholder='Enter Your Email Id'
-                        name = "email"
-                        value={email}
-                        onChange={(e)=>onInputChange(e)}
-                        required
-                        />
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor='PhoneNumber' className='form-lable'>
-                        Phone Number
-                        </label>
-                        <input
-                        type={'number'}
-                        className='form-control'
-                        placeholder='Enter Your Phone Number'
-                        name = "phonenumber"
-                        value={phonenumber}
-                        onChange={(e)=>onInputChange(e)}
-                        required
-                        />
-                    </div>
-                    <button className='btn btn-outline-primary mx-2'>Submit <FontAwesomeIcon icon={faArrowRightToBracket} /></button>
-                    <Link className='btn btn-outline-danger mx-2' to="/">Cancel <FontAwesomeIcon icon={faXmark} /></Link>
-                </form>
+    <div className="conntainer">
+      <div className="row">
+        <div className="col-md-6 offset-3 border rounded p-4 mt-2 shadow custom_table_style">
+          <h2 className="text-center m-4">Edit User</h2>
+          <form onSubmit={(e) => onSubmit(e)}>
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-lable">
+                Name
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter Your Name"
+                name="name"
+                value={name}
+                onChange={(e) => onInputChange(e)}
+                required
+              />
             </div>
+            <div className="mb-3">
+              <label htmlFor="Username" className="form-lable">
+                UserName
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter Your UserName"
+                name="username"
+                value={username}
+                onChange={(e) => onInputChange(e)}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Email" className="form-lable">
+                Email
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter Your Email Id"
+                name="email"
+                value={email}
+                onChange={(e) => onInputChange(e)}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="PhoneNumber" className="form-lable">
+                Phone Number
+              </label>
+              <input
+                type={"number"}
+                className="form-control"
+                placeholder="Enter Your Phone Number"
+                name="phonenumber"
+                value={phonenumber}
+                onChange={(e) => onInputChange(e)}
+                required
+              />
+            </div>
+            <button className="btn btn-outline-primary mx-2">
+              Submit <FontAwesomeIcon icon={faArrowRightToBracket} />
+            </button>
+            <Link className="btn btn-outline-danger mx-2" to="/">
+              Cancel <FontAwesomeIcon icon={faXmark} />
+            </Link>
+          </form>
         </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/users/ViewUser.js
+++ src/users/ViewUser.js
@@ -1,88 +1,100 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { Link, useParams, useNavigate } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare,faHouse, faTrash} from '@fortawesome/free-solid-svg-icons';
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPenToSquare,
+  faHouse,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function ViewUser() {
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+    phonenumber: null,
+    date: null,
+    time: null,
+  });
 
-    const ROOT_URL = "http://localhost:8081/";
-    const [user, setUser] = useState({
-        name:"",
-        username:"",
-        email:"",
-        phonenumber:null,
-        date:null,
-        time:null
-    });
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-    const {id} = useParams();
-    const navigate = useNavigate();
+  const deleteUser = async (id, username) => {
+    await axios.delete(`/deleteuser/${id}`);
+    navigate("/", { replace: true });
+  };
 
+  useEffect(() => {
+    loadUser();
+  }, []);
 
-    const deleteUser = async (id,username) => {
-        await axios.delete(ROOT_URL+`deleteuser/${id}`);
-        navigate('/', { replace: true });
-    }
-
-    useEffect(()=>{
-        loadUser()
-    },[]);
-
-    const loadUser = async()=>{
-        const result=await axios.get(ROOT_URL+`user/${id}`);
-        setUser(result.data);
-    };
+  const loadUser = async () => {
+    const result = await axios.get(`/user/${id}`);
+    setUser(result.data);
+  };
 
   return (
-    <div className='conntainer'>
-        <div className='row'>
-            <div className="col-md-6 offset-3 border rounded p-4 mt-2 shadow custom_table_style">
-            <h2 className='text-center m-4'>View User</h2>
+    <div className="conntainer">
+      <div className="row">
+        <div className="col-md-6 offset-3 border rounded p-4 mt-2 shadow custom_table_style">
+          <h2 className="text-center m-4">View User</h2>
 
-            <div className='card'>
-                <div className='card-header'>
-                    Details of User with ID : {user.id}
-                    <ul className='list-group list-group-flush mt-2'>
-                        <li className='list-group-item'>
-                            <b>Name : </b>
-                            {user.name}
-                        </li>
-                        <li className='list-group-item'>
-                            <b>UserName : </b>
-                            {user.username}
-                        </li>
-                        <li className='list-group-item'>
-                            <b>Email : </b>
-                            {user.email}
-                        </li>
-                        <li className='list-group-item'>
-                            <b>Phone Number : </b>
-                            {user.phonenumber}
-                        </li>
-                        <li className='list-group-item'>
-                            <b>Date : </b>
-                            {user.date}
-                        </li>
-                        <li className='list-group-item'>
-                            <b>Time : </b>
-                            {user.time}
-                        </li>
-                    </ul>
-                </div>
+          <div className="card">
+            <div className="card-header">
+              Details of User with ID : {user.id}
+              <ul className="list-group list-group-flush mt-2">
+                <li className="list-group-item">
+                  <b>Name : </b>
+                  {user.name}
+                </li>
+                <li className="list-group-item">
+                  <b>UserName : </b>
+                  {user.username}
+                </li>
+                <li className="list-group-item">
+                  <b>Email : </b>
+                  {user.email}
+                </li>
+                <li className="list-group-item">
+                  <b>Phone Number : </b>
+                  {user.phonenumber}
+                </li>
+                <li className="list-group-item">
+                  <b>Date : </b>
+                  {user.date}
+                </li>
+                <li className="list-group-item">
+                  <b>Time : </b>
+                  {user.time}
+                </li>
+              </ul>
             </div>
-            <Link className="btn btn-primary my-3 mx-2" to={"/"}><FontAwesomeIcon icon={faHouse} /></Link>
-            <Link className='btn btn-success mx-2' to={`/edituser/${user.id}`}><FontAwesomeIcon icon={faPenToSquare} /></Link>
-            <button className='btn btn-danger mx-2' onClick={() => {
-                const confirmBox = window.confirm( "Do you really want to delete this USER - " + user.username +" ?")
-                    if (confirmBox === true){
-                        deleteUser(user.id,user.username)
-                    }}}><FontAwesomeIcon icon={faTrash} />
-            </button>
-
-            </div>
+          </div>
+          <Link className="btn btn-primary my-3 mx-2" to={"/"}>
+            <FontAwesomeIcon icon={faHouse} />
+          </Link>
+          <Link className="btn btn-success mx-2" to={`/edituser/${user.id}`}>
+            <FontAwesomeIcon icon={faPenToSquare} />
+          </Link>
+          <button
+            className="btn btn-danger mx-2"
+            onClick={() => {
+              const confirmBox = window.confirm(
+                "Do you really want to delete this USER - " +
+                  user.username +
+                  " ?"
+              );
+              if (confirmBox === true) {
+                deleteUser(user.id, user.username);
+              }
+            }}
+          >
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
         </div>
+      </div>
     </div>
   );
 }
